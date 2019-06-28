c1d0b82e316e3aad42937c10e5934a8cfacf0f85
Diwakar Bhatt <diwakar.bhatt@freecharge.com>
2019-06-28 11:52:11 +0530

    SMS Broadcast reciever

---
+++ android/app/src/main/java/com/moneymanager/reciever/IncomingSms.java
@@ -0,0 +1,39 @@
+import android.content.*;
+import android.telephony.*;
+import android.os.*;
+import android.util.*;
+
+public class IncomingSms extends BroadcastReceiver {
+
+    // Get the object of SmsManager
+    final SmsManager sms = SmsManager.getDefault();
+
+    public void onReceive(Context context, Intent intent) {
+
+        // Retrieves a map of extended data from the intent.
+        final Bundle bundle = intent.getExtras();
+
+        try {
+
+            if (bundle != null) {
+
+                final Object[] pdusObj = (Object[]) bundle.get("pdus");
+
+                for (int i = 0; i < pdusObj.length; i++) {
+
+                    SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
+                    String phoneNumber = currentMessage.getDisplayOriginatingAddress();
+
+                    String senderNum = phoneNumber;
+                    String message = currentMessage.getDisplayMessageBody();
+
+                    Log.i("SmsReceiver", "senderNum: "+ senderNum + "; message: " + message);
+                } // end for loop
+              } // bundle is null
+
+        } catch (Exception e) {
+            Log.e("SmsReceiver", "Exception smsReceiver" +e);
+
+        }
+    }
+}
---
+++ component/pushsms/PushSms.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { Text, TextInput, View, Picker, DatePickerAndroid, Button } from 'react-native';
+
+export default class PushSms extends Component {
+
+
+
+}
