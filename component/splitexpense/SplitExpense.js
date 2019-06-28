9c6d37703a53eb9ea2c2e1b339c87a4aeaeb1e4b
akashayan <akash@freecharge.com>
2019-06-29 02:14:58 +0530

    split expense page

--- component/splitexpense/SplitExpense.js
+++ component/splitexpense/SplitExpense.js
@@ -61,11 +61,16 @@ export default class SplitExpense extends Component {
 
         this.setState(() => {
             return {
+                amt: "Split amount : Rs. "+(this.totalAmount/(this.divisors)).toFixed(2),
                 valueArray: newArray
+                /*valueArray: newArray,
+                amt: "Split amount : Rs. "+(this.totalAmount/(this.divisors+1)).toFixed(2)*/
+
             }
         }, () => {
             LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
-        });
+
+        },);
     }
 
     render() {
